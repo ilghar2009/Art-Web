c597c7efbfc62935207d79b23835863234adcd9f
ilghar <ilghar09.88@gmail.com>
2025-02-17 20:35:06 +0330

    create migrations

---
+++ laravel11-app/database/migrations/2025_02_17_165302_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->string('comment_id');
+            $table->string('created_by');
+            $table->string('product_id')->nullable();
+            $table->string('comment_id')->nullable();
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
